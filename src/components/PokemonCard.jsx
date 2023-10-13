f6eba6ba449e4d48c086ddea5e90e5967f89284e
Kino <faiza.fehri+github@gmail.com>
2023-10-13 20:11:22 +0100

    challenge 3 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt ="Bulbasaur"
-            >
-            </img>
-            <figcaption> Bulbasaur</figcaption>
-        </figure>
-        );
+
+const pokemonList = [
+  {
+    name: 'bulbasaur',
+    imgSrc:
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+];
+const PokemonCard = () =>{
+  const pokemonElement = pokemonList.map((pokemon) => (
+    pokemon.imgSrc !== undefined ? (
+      <figure>
+        <img src={pokemon.imgSrc} alt={pokemon.name.toUpperCase()} />
+        <figcaption>{pokemon.name.toUpperCase()}</figcaption>
+      </figure>
+    ) : (
+      <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name.toUpperCase()}</figcaption>
+      </figure>
+    )
+  ));
+  return (pokemonElement);
 }
 
 export default PokemonCard;
